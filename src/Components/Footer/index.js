5c321fcaa77e82547a61fb854cfba4f1c0308909
unknown <mauricio_lopezs@hotmail.com>
2020-09-04 11:44:09 -0500

    updates-text-css

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,11 +3,11 @@ import { Container, Row, Col } from 'reactstrap'
 
 import './Footer.css'
 
-import Facebook from './img/facebook 1.png'
-import Twitter from './img/Twitter 1.png'
-import pinterest from './img/pinterest 1.png'
-import instagram from './img/instagram 1.png'
-import stripe from './img/stripe.png'
+import facebooki from './img/facebooki.png'
+import twitter from './img/twitter.png'
+import pinterest from './img/Pinterest.png'
+import instagram from './img/instagram.png'
+import tarjetas from './img/tarjetas.png'
 import Ubicacion from './img/Ubicacion.png'
 
 const Footer = () => {
@@ -21,16 +21,17 @@ const Footer = () => {
 
       <div className="row row-cols-2">
         <div className="col">
-          <img src={stripe} className="pago" alt="..." />
+          <img src={tarjetas} className="pago" alt="..." />
         </div>
         <div class="col">
           <Col className="icons-container col-md-6 col-ms-12">
-            <img className="img-modi" src={Facebook} alt="img" />
-            <img className="img-modi" src={Twitter} alt="img" />
+            <img className="img-modi" src={facebooki} alt="img" />
+            <img className="img-modi" src={twitter} alt="img" />
             <img className="img-modi" src={pinterest} alt="img" />
             <img className="img-modi" src={instagram} alt="img" />
           </Col>
         </div>
+      
         <div class="col"></div>
 
         <div class="col">
@@ -38,10 +39,11 @@ const Footer = () => {
           {/* <p className="foo">Potilica de privaciad</p> */}
         </div>
       </div>
+      <hr className="border-f"></hr>
       <div class="col">
         <p className="foo-1-all">
-          © 2020 Boss Repair, Inc. Todos los derechos reservados
-          &nbsp; &nbsp; México
+          © 2020 Boss Repair, Inc. Todos los derechos reservados &nbsp; &nbsp;
+          México
           <img src={Ubicacion} className="ubi" alt="..." />
         </p>
       </div>
